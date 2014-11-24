efd3494ae494f2ed90c94842ab2e5e4f014f0445
Michaelangelo Jong <mjong@compilr.com>
2014-11-23 20:36:11 -0400

    Finalize the generator API model.

--- generate.js
+++ generate.js
@@ -23,6 +23,73 @@ function getFunctionName(func) {
 }
 
 /**
+ * Returns true if 'obj' is an object containing only get and set functions, false otherwise.
+ * @param  {Any} obj Value to be tested.
+ * @return {Boolean} true or false.
+ */
+function isGetSet(obj) {
+    var keys, length;
+    if (obj instanceof Object) {
+        keys = Object.getOwnPropertyNames(obj).sort();
+        length = keys.length;
+
+        if ((length === 1 && (keys[0] === 'get' && obj.get instanceof Function ||
+                              keys[0] === 'set' && obj.set instanceof Function)) ||
+            (length === 2 && (keys[0] === 'get' && obj.get instanceof Function &&
+                              keys[1] === 'set' && obj.set instanceof Function))) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * Defines properties on 'obj'.
+ * @param  {Object} obj        An object that 'properties' will be attached to.
+ * @param  {Object} descriptor Optional object descriptor that will be applied to all attaching properties on 'properties'.
+ * @param  {Object} properties An object who's properties will be attached to 'obj'.
+ * @return {Generator}         'obj'.
+ */
+function defineObjectProperties(obj, descriptor, properties) {
+    var setProperties = {},
+        i,
+        keys,
+        length;
+
+    if (!descriptor || !(descriptor instanceof Object)) {
+        descriptor = {};
+    }
+
+    if (!properties || !(properties instanceof Object)) {
+        properties = descriptor;
+        descriptor = {};
+    }
+
+    keys = Object.getOwnPropertyNames(properties);
+    length = keys.length;
+
+    for (i = 0; i < length; i++) {
+        if (isGetSet(properties[keys[i]])) {
+            setProperties[keys[i]] = {
+                configurable: !!descriptor.configurable,
+                enumerable: !!descriptor.enumerable,
+                get: properties[keys[i]].get,
+                set: properties[keys[i]].set
+            };
+        } else {
+            setProperties[keys[i]] = {
+                configurable: !!descriptor.configurable,
+                enumerable: !!descriptor.enumerable,
+                writable: !!descriptor.writable,
+                value: properties[keys[i]]
+            };
+        }
+    }
+    Object.defineProperties(obj, setProperties);
+    return obj;
+}
+
+/**
  * Generates a new generator that inherits from 'ParentGenerator'.
  * @param {Generator} ParentGenerator Generator to inherit from.
  * @param {Function} create           Create method that gets called when creating a new instance of new generator.
@@ -38,129 +105,120 @@ function GeneratorFunc(ParentGenerator, create, init) {
     create = typeof create === 'function' ? create : Generator.proto.__create;
     init   = typeof init   === 'function' ? init   : Generator.proto.__init;
 
-    Object.defineProperties(properties, {
-        proto: {
-            value: ParentGenerator.proto.prototypeProperties
+    defineObjectProperties(
+        properties,
+        {
+            configurable: false,
+            enumerable: false,
+            writable: false
         },
-        generator: {
-            value: generator
+        {
+            proto: ParentGenerator.proto.prototypeProperties,
+            generator: generator
         }
-    });
+    );
 
-    Object.defineProperties(proto, {
-        proto: {
-            value: ParentGenerator.proto
+    defineObjectProperties(
+        proto,
+        {
+            configurable: false,
+            enumerable: false,
+            writable: false
         },
-        prototypeProperties: {
-            value: properties
-        },
-        create: {
-            set: function (createFunc) {
-                if (typeof createFunc === 'function') {
-                    create = createFunc;
-                }
-                return Generator.proto.create;
-            },
-            get: function () {
-                return Generator.proto.create;
-            }
-        },
-        __create: {
-            set: function (createFunc) {
-                if (typeof createFunc === 'function') {
-                    create = createFunc;
-                }
-                return create;
-            },
-            get: function () {
-                return create;
-            }
-        },
-        generate: {
-            get: function () {
-                return Generator.proto.generate;
-            }
-        },
-        init: {
-            set: function (initFunc) {
-                if (typeof initFunc === 'function') {
-                    init = initFunc;
-                }
-                return Generator.proto.init;
-            },
-            get: function () {
-                return Generator.proto.init;
-            }
-        },
-        __init: {
-            set: function (initFunc) {
-                if (typeof initFunc === 'function') {
-                    init = initFunc;
-                }
-                return init;
-            },
-            get: function () {
-                return init;
-            }
+        {
+            proto: ParentGenerator.proto,
+            prototypeProperties: properties,
+            __create: create,
+            __init: init
         }
-    });
+    );
 
-    Object.defineProperty(generator, 'proto', {
-        value: proto
-    });
+    defineObjectProperties(
+        generator,
+        {
+            configurable: false,
+            enumerable: false,
+            writable: false
+        },
+        {
+            proto: proto
+        }
+    );
 
     return generator;
 }
 
-// Generator Base Inheritance
-Object.defineProperties(GeneratorProto, {
-    prototypeProperties: {
-        value: GeneratorMethods
+// Generator Instance Inheritance
+defineObjectProperties(
+    GeneratorMethods,
+    {
+        configurable: false,
+        enumerable: false,
+        writable: false
     },
-    create: {
-        value:
+    {
+        /**
+         * Defines properties on this object.
+         * @param  {Object} descriptor Optional object descriptor that will be applied to all attaching properties.
+         * @param  {Object} properties An object who's properties will be attached to this object.
+         * @return {Object}            This object.
+         */
+        defineProperties: function defineProperties(descriptor, properties) {
+            defineObjectProperties(this, descriptor, properties);
+            return this;
+        }
+    }
+);
+
+// Generator Base Inheritance
+defineObjectProperties(
+    GeneratorProto,
+    {
+        configurable: false,
+        enumerable: false,
+        writable: false
+    },
+    {
+        prototypeProperties: GeneratorMethods,
         /**
          * Creates a new instance of this Generator.
          * @return {Generator} Instance of this Generator.
          */
-        function create() {
+        create: function create() {
             var newObj = Object.create(this.proto.prototypeProperties);
 
-            Object.defineProperties(newObj, {
-                proto: {
-                    value: this.proto.prototypeProperties
+            newObj.defineProperties(
+                {
+                    configurable: false,
+                    enumerable: false,
+                    writable: false
+                },
+                {
+                    proto: this.proto.prototypeProperties
                 }
-            });
+            );
 
             this.init(newObj);
 
             this.__create.apply(newObj, Array.prototype.slice.call(arguments));
             return newObj;
-        }
-    },
-    __create: {
-        value: function () {}
-    },
-    generate: {
-        value:
+        },
+        __create: function () {},
         /**
          * Returns a new Generator that inherits from this Generator.
          * @param {Function} create Create method that gets called when creating a new instance of new generator.
          * @param {Function} init   Inits any data stores needed by prototypal methods.
          * @return {Generator}      New generator that inherits from this Generator.
          */
-        function generate(create, init) {
+        generate:function generate(create, init) {
             return GeneratorFunc(this, create, init);
-        }
-    },
-    init: {
-        value:
+        },
         /**
          * Inits any data stores on 'obj' needed by prototypal methods.
          * @param  {Object} obj A Generator instance.
          * @return {undefined}  undefined.
          */
-        function init(obj) {
+        init: function init(obj) {
             var me = this,
                 inits = [],
                 i;
@@ -171,19 +229,14 @@ Object.defineProperties(GeneratorProto, {
             for (i = inits.length - 1; i >= 0; i--) {
                 inits[i].call(obj);
             }
-        }
-    },
-    __init: {
-        value: function () {}
-    },
-    isGeneration: {
-        value:
+        },
+        __init: function() {},
         /**
          * Returns true if 'generator' was generated by this Generator.
          * @param  {Generator} generator A Generator.
          * @return {Boolean}             true or false.
          */
-        function isGeneration(generator) {
+        isGeneration: function isGeneration(generator) {
             var _ = this;
             if (generator instanceof Object && _ !== generator) {
                 while (typeof generator.proto === 'object') {
@@ -194,16 +247,13 @@ Object.defineProperties(GeneratorProto, {
                 }
             }
             return false;
-        }
-    },
-    isCreation: {
-        value:
+        },
         /**
          * Returns true if 'object' was created by this Generator.
          * @param  {Object} object An Object.
          * @return {Boolean}       true or false.
          */
-        function isCreation(object) {
+        isCreation: function isCreation(object) {
             var _ = this;
             if (object instanceof Object && _ !== object) {
                 while (typeof object.proto === 'object') {
@@ -214,66 +264,73 @@ Object.defineProperties(GeneratorProto, {
                 }
             }
             return false;
-        }
-    },
-    definePrototypeMethods: {
-        value:
+        },
         /**
          * Defines methods on this' prototype.
          * @param  {Array} methods An array of named methods.
          * @return {Generator}     This Generator.
          */
-        function definePrototypeMethods(descriptor, methods) {
-            var prototypeProperties = {},
+        definePrototypeMethods: function definePrototypeMethods(descriptor, methods) {
+            var setMethods = {},
                 i;
-            if (descriptor instanceof Array) {
+            if (!methods || !(methods instanceof Object)) {
                 methods = descriptor;
                 descriptor = {};
             }
+
+            if (!descriptor || !(descriptor instanceof Object)) {
+                descriptor = {};
+            }
+
             if (methods instanceof Array) {
                 for (i = 0; i < methods.length; i++) {
                     if (typeof methods[i] === 'function') {
-                        prototypeProperties[getFunctionName(methods[i])] = {
-                            configurable: !!descriptor.configurable,
-                            enumerable: !!descriptor.enumerable,
-                            writable: !!descriptor.writable,
-                            value: methods[i]
-                        };
+                        setMethods[getFunctionName(methods[i])] = methods[i];
                     }
                 }
-                Object.defineProperties(this.proto.prototypeProperties, prototypeProperties);
+
             }
+            this.definePrototype(descriptor, setMethods);
+            console.warn('Generator.definePrototypeMethods is deprecated, please use \'definePrototype\' instead.');
             return this;
-        }
-    },
-    definePrototypeProperties: {
-        value:
+        },
         /**
          * Defines shared properties for all instances of this.
          * @param  {Object} properties Object keys => descriptors.
          * @return {Generator}         This Generator.
          */
-        function definePrototypeProperties(properties) {
-            if (properties instanceof Object) {
-                Object.defineProperties(this.proto.prototypeProperties, properties);
-            }
+        definePrototypeProperties: function definePrototypeProperties(descriptor, properties) {
+            this.definePrototype(descriptor, properties);
+            console.warn('Generator.definePrototypeProperties is deprecated, please use \'definePrototype\' instead.');
+            return this;
+        },
+        /**
+         * Defines shared properties for all objects created by this generator.
+         * @param  {Object} descriptor Optional object descriptor that will be applied to all attaching properties.
+         * @param  {Object} properties An object who's properties will be attached to this generator's prototype.
+         * @return {Generator}         This generator.
+         */
+        definePrototype: function definePrototype(descriptor, properties) {
+            defineObjectProperties(this.proto.prototypeProperties, descriptor, properties);
             return this;
         }
     }
-});
+);
 
 // Generator Class Methods
-Object.defineProperties(Generator, {
-    proto: {
-        value: GeneratorProto
+defineObjectProperties(
+    Generator,
+    {
+        configurable: false,
+        enumerable: false,
+        writable: false
     },
-    generate: {
-        value: GeneratorProto.generate
-    },
-    isGenerator: {
-        value: GeneratorProto.isGeneration
+    {
+        proto:       GeneratorProto,
+        generate:    GeneratorProto.generate,
+        isGenerator: GeneratorProto.isGeneration
     }
-});
+);
 
 // Exports
 module.exports = Generator;
